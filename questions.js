bbb4d1eed83ab52f21301546742dc6055aa68d51
Kenneth <hello@kennethmckenzie.co.uk>
2023-02-04 13:00:21 +0000

    Added questions

---
+++ questions.js
@@ -0,0 +1,118 @@
+export const questions = [
+    {
+        question:'What does CSS stand for?', 
+        answers: [
+            {answer: 'Cascading Style Sheets', correctAnswer: true },
+            {answer: 'Colorful Style Sheets', correctAnswer: false },
+            {answer: 'Computer Style Sheets', correctAnswer: false},
+            {answer: 'Creative Style Sheets', correctAnswer: false }
+        ]
+
+    },
+    {
+        question:'Where in an HTML document is the correct place to refer to an external style sheet (style.css)?', 
+        answers: [
+            {answer: 'At the end of the document', correctAnswer: false },
+            {answer: 'In the <body> section', correctAnswer: false },
+            {answer: 'In the <head> section', correctAnswer: true }
+        ]
+
+    },
+    {
+        question:'What is the correct HTML for referring to an external style sheet? (linking your HTML and Style sheet together)', 
+        answers: [
+            {answer: '<stylesheet>mystyle.css</stylesheet>', correctAnswer: false },
+            {answer: '<style src="mystyle.css">', correctAnswer: false },
+            {answer: '<link rel="stylesheet" type="text/css" href="mystyle.css">', correctAnswer: true }
+        ]
+
+    },
+    {
+        question:'Which property is used to change the background color?', 
+        answers: [
+            {answer: 'background-color', correctAnswer: true },
+            {answer: 'color', correctAnswer: false },
+            {answer: 'bgcolor', correctAnswer: false },
+            {answer: 'border-color', correctAnswer: false }
+        ]
+
+    },
+    {
+        question:'Which CSS property controls the text size?', 
+        answers: [
+            {answer: 'text-size', correctAnswer: false },
+            {answer: 'font-size', correctAnswer: true },
+            {answer: 'text-style', correctAnswer: false },
+            {answer: 'font-style', correctAnswer: false }
+        ]
+
+    },
+    {
+        question:'Which property is used to change the font of an element?', 
+        answers: [
+            {answer: 'font-style', correctAnswer: false },
+            {answer: 'font-family', correctAnswer: true },
+            {answer: 'font-weight', correctAnswer: false }
+        ]
+
+    },
+    {
+        question:'Which of the following is a component of CSS style rule? (Select all that apply)', 
+        answers: [
+            {answer: 'Selector', correctAnswer: true },
+            {answer: 'Property', correctAnswer: true },
+            {answer: 'Value', correctAnswer: true },
+        ]
+
+    },
+    {
+        question:'Which of the following property is used to set the height of an image?', 
+        answers: [
+            {answer: 'border', correctAnswer: false },
+            {answer: 'height', correctAnswer: true },
+            {answer: 'width', correctAnswer: false },
+            {answer: 'size', correctAnswer: false }
+        ]
+
+    },
+    {
+        question:'Which of the following property specifies the color of the whole border?', 
+        answers: [
+            {answer: 'border-color', correctAnswer: true },
+            {answer: 'border-style', correctAnswer: false },
+            {answer: 'border-width', correctAnswer: false },
+            {answer: 'border-bottom-color', correctAnswer: false }
+        ]
+
+    },
+    {
+        question:'What does a hyperlink do? (Select all that apply)', 
+        answers: [
+            {answer: 'Takes you to another web page', correctAnswer: true },
+            {answer: 'Closes the Internet browser', correctAnswer: false },
+            {answer: 'Opens the Internet browser', correctAnswer: false },
+            {answer: 'Takes you to another website', correctAnswer: true}
+        ]
+
+    },
+    {
+        question:'Where to upload the website after it has been created?', 
+        answers: [
+            {answer: 'Client', correctAnswer: false },
+            {answer: 'Server', correctAnswer: true },
+            {answer: 'Desktop', correctAnswer: false },
+            {answer: 'Drive', correctAnswer: false }
+        ]
+
+    },
+    {
+        question:'Style sheets can be?', 
+        answers: [
+            {answer: 'Internal', correctAnswer: false },
+            {answer: 'External', correctAnswer: false },
+            {answer: 'Both', correctAnswer: true },
+            {answer: 'None', correctAnswer: false }
+        ]
+
+    }
+]
--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-// Nested arrays?, Start button 
+// Start button, next button, pagination , overwrite each question, score at the end. show wrong answers, percentage
+
+import {questions} from "./questions.js";
+
